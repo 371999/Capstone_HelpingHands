aae021a0c1a69ded6ee273aa44745ca2500c90bc
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-10-30 14:43:45 -0400

    Update main.tf

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 
   client_id       = var.client_id
   client_secret   = var.client_secret
-  subscription_id = var.subscription_id
+  subscription_id = var.subscription_id 
   tenant_id       = var.tenant_id
 }
 
