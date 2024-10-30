13c3c5348ce934db84da7100be24c72e53abc837
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-10-30 15:01:04 -0400

    Update main.tf

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -1,14 +1,19 @@
 provider "azurerm" {
   features {}
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id 
-  tenant_id       = var.tenant_id
+  ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+  ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+  ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+  ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+
 }
 
 terraform {  
   backend "azurerm" {
+    ARM_CLIENT_ID: ${{ secrets.AZURE_CLIENT_ID }}
+    ARM_CLIENT_SECRET: ${{ secrets.AZURE_CLIENT_SECRET }}
+    ARM_TENANT_ID: ${{ secrets.AZURE_TENANT_ID }}
+    ARM_SUBSCRIPTION_ID: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
     resource_group_name  = "myBackendResourceGroup"  
     storage_account_name = "shreyas3799"        
     container_name       = "tfstate"                 
