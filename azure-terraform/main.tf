8fe11b33ec40498c3bd93f4d4514411a1c1be5e6
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-01 14:03:26 -0400

    Update main.tf

--- azure-terraform/main.tf
+++ azure-terraform/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
   features {}
 
   # Use variables or environment variables for authentication
-  client_id       = var.client_id
+  client_id       = var.client_id 
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
