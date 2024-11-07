ebc07862b4e6c1640cd7208b0bd06d3ed9509588
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2024-11-06 21:14:42 -0500

    Create ItemRoute.js file to update the user profile record

---
+++ backend/routes/ItemRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { createItem, getItemById, deleteItemById, updateItemById, getItems } = require("../controllers/ItemController");
+const router = express.Router();
+
+//Routes for user profile record
+router.get('/:id', getItemById);
+router.get('/', getItems);
+router.post("/", createItem);
+router.put('/:id', updateItemById);
+
+module.exports = router;
