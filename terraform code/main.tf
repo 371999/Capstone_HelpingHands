ecd70ac14c3684d3341338b4e7268ff01f0f1af1
snshah16 <sarthaknshah1998@gmail.com>
2024-11-06 23:28:03 -0500

    update in main.tf

---
+++ terraform code/main.tf
@@ -0,0 +1,189 @@
+# Define the Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Configure the Terraform backend to store state in Azure
+terraform {   
+  backend "azurerm" {  
+    resource_group_name  = "myBackendResourceGroup"  
+    storage_account_name = "shreyas3799"        
+    container_name       = "tfstate"                 
+    key                  = "dev.tfstate"             
+  }
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "dev_rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "dev_vnet" {
+  name                = "dev-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.dev_rg.location
+  resource_group_name = azurerm_resource_group.dev_rg.name
+}
+
+# Create a Subnet
+resource "azurerm_subnet" "dev_subnet" {
+  name                 = "dev-subnet"
+  resource_group_name  = azurerm_resource_group.dev_rg.name
+  virtual_network_name = azurerm_virtual_network.dev_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a Network Security Group
+resource "azurerm_network_security_group" "dev_nsg" {
+  name                = "dev-nsg"
+  location            = azurerm_resource_group.dev_rg.location
+  resource_group_name = azurerm_resource_group.dev_rg.name
+
+  # Allow SSH
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTP and HTTPS
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow custom port (8080) if needed
+  security_rule {
+    name                       = "Allow-8080"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create a Public IP
+resource "azurerm_public_ip" "dev_public_ip" {
+  name                = "dev-public-ip"
+  location            = azurerm_resource_group.dev_rg.location
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Create a Network Interface
+resource "azurerm_network_interface" "dev_nic" {
+  name                = "dev-nic"
+  location            = azurerm_resource_group.dev_rg.location
+  resource_group_name = azurerm_resource_group.dev_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.dev_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.dev_public_ip.id
+  }
+}
+
+# Associate the Network Security Group with the Network Interface
+resource "azurerm_network_interface_security_group_association" "dev_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.dev_nic.id
+  network_security_group_id = azurerm_network_security_group.dev_nsg.id
+}
+
+# Create a Virtual Machine
+resource "azurerm_linux_virtual_machine" "dev_vm" {
+  name                = "dev-vm"
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  location            = azurerm_resource_group.dev_rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.ssh_public_key
+  }
+
+  network_interface_ids = [azurerm_network_interface.dev_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+# Create an Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "myContainerRegistry123"
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  location            = azurerm_resource_group.dev_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Define a variable for SSH public key
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+}
+
+# Output the Public IP and ACR login server details
+output "vm_public_ip" {
+  value       = azurerm_public_ip.dev_public_ip.ip_address
+  description = "The public IP of the VM"
+}
+
+output "acr_login_server" {
+  value       = azurerm_container_registry.acr.login_server
+  description = "The login server for Azure Container Registry"
+}
+
+output "acr_username" {
+  value       = azurerm_container_registry.acr.admin_username
+  description = "The username for Azure Container Registry"
+  sensitive   = true
+}
+
+output "acr_password" {
+  value       = azurerm_container_registry.acr.admin_password
+  description = "The password for Azure Container Registry"
+  sensitive   = true
+}
