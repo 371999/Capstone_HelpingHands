27b1f4e0e3ebc919c395ddd1d2d4dad57d6d31f8
snshah16 <sarthaknshah1998@gmail.com>
2024-11-07 15:13:39 -0500

    Update variable.tf

--- terraform_code/variable.tf
+++ terraform_code/variable.tf
@@ -16,3 +16,24 @@ variable "ssh_public_key" {
   type        = string
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDylXVW19PpUkYTNzPkvh98eA2XanpqNI2tyvVJ40HXbWG+feV04/N0OQgjymzrg1sWhbiNCSYVJi9RBmZKd5OkdXgV2qPnKKM1u8/D10q/QAKCZE1pm/3NU/CJrzpFTQTMnNo8sh0SRYiXWTrb2AlE08Eizjizns3hUml3Tx4M5+SedVzzKFizwdltVE7ZCNWzhHLzIeVdYQ6Z8BjI1fcddkYIxCMqqSvH4nJ3DuurpJyWVCrppBtXpOooX2VscO8zd+R9YfBRusZC6wthyWVRiseRwivUk4iGx+8eRVbDOES+FQ9XSAmYNI9lISU6tMlytpfG9doYVcfs67Fig/nfyKyWEGzE3mGmAtZMrZ05RAnnJ1exmvfIiNK2sBfF72CeYNPjgr2yUHYVqnyDvE/purXcnWstaLuYqMMota9z3SGOY2+SoKfXDYdYMIC03m35AXeYiA4+oqSe2i908wMiaams2BsdOAxWyyVHfO5jcSiJc38h5RmgcMYbTiWLHFzqvee8hjop81Okre99eTRPq3GHeQ6Lu83yAnY2TciJObYvrYTrGNZ6g2EVtoONW/FyESzScoziUuOlhg5MG7QSXfNrfH58WZDYVAyvksqx545xA5T1SBmteH+ASu6t77Mo/hvv8/3Qvqh5WOeqOw0B1sMoOph2EYU8i3nPK3Jr9Q== project"
 }
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
