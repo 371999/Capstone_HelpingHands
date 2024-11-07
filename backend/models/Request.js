3a73164b1c610f0e63fd3f88e81db9bfb554d1ba
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2024-11-06 21:04:38 -0500

    Created Request.js to store the data of requested item to database

---
+++ backend/models/Request.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const RequestSchema = new mongoose.Schema({
+    requestType: {
+        type: String,
+        required: true,
+        enum: ['delivery', 'pick up'], // Limits to these two types
+    },
+    userId: {
+        type: String,
+        required: true, // Ensure the request is tied to a specific user
+    },
+    itemId: {
+        type: String, // Store itemId as a string
+        required: true,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Request', RequestSchema);
