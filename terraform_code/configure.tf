430923d9db3aafae86ddfde6737072291a950749
snshah16 <sarthaknshah1998@gmail.com>
2024-11-07 09:30:15 -0500

    Update configure.tf

--- terraform_code/configure.tf
+++ terraform_code/configure.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "myBackendResourceGroup"   # Hardcoded name of the resource group
-    storage_account_name  = "sarthak"              # Hardcoded name of the storage account
+    storage_account_name  = "sarthak12345storage"              # Hardcoded name of the storage account
     container_name        = "tfstate"                  # Hardcoded name of the storage container
     key                   = "dev.tfstate"
   }
