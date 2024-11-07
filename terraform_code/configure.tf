e1534c263449e1d13f952f970a92001a8bed2bc4
snshah16 <sarthaknshah1998@gmail.com>
2024-11-07 15:13:25 -0500

    Update configure.tf

--- terraform_code/configure.tf
+++ terraform_code/configure.tf
@@ -1,7 +1,13 @@
 # Provider Configuration for Azure
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
+
 terraform {
   backend "azurerm" {
     resource_group_name   = "myBackendResourceGroup"   # Hardcoded name of the resource group
