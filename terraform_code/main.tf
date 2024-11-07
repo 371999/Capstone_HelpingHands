35d481856b84c627ec57cd0a2eb485accbfc6034
snshah16 <sarthaknshah1998@gmail.com>
2024-11-07 09:03:14 -0500

    Update main.tf removed variables and configuration part

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -1,68 +1,10 @@
-# Step 1: Define Azure Provider
-provider "azurerm" {
-  features {}
-}
-
-# Step 2: Create Resource Group and Storage Account for Backend State
-resource "azurerm_resource_group" "backend_rg" {
-  name     = "myBackendResourceGroup"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "backend_storage" {
-  name                     = "sarthak"         # Change to a globally unique name if needed
-  resource_group_name      = azurerm_resource_group.backend_rg.name
-  location                 = azurerm_resource_group.backend_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.backend_storage.name
-  container_access_type = "private"
-}
-
-# Step 3: Configure Terraform Backend (after running terraform apply once)
-terraform {
-  backend "azurerm" {
-    resource_group_name   = azurerm_resource_group.backend_rg.name
-    storage_account_name  = azurerm_storage_account.backend_storage.name
-    container_name        = azurerm_storage_container.tfstate.name
-    key                   = "dev.tfstate"
-  }
-}
-
-# Step 4: Create Other Resources (e.g., VM, Network, etc.)
-
-# Define a new resource group for other resources
-resource "azurerm_resource_group" "dev_rg" {
-  name     = "myResourceGroup"
-  location = "East US"
-}
-
-# Create a Virtual Network
-resource "azurerm_virtual_network" "dev_vnet" {
-  name                = "dev-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.dev_rg.location
-  resource_group_name = azurerm_resource_group.dev_rg.name
-}
-
-# Create a Subnet
-resource "azurerm_subnet" "dev_subnet" {
-  name                 = "dev-subnet"
-  resource_group_name  = azurerm_resource_group.dev_rg.name
-  virtual_network_name = azurerm_virtual_network.dev_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create a Network Security Group
+# Network Security Group with All Security Rules
 resource "azurerm_network_security_group" "dev_nsg" {
   name                = "dev-nsg"
   location            = azurerm_resource_group.dev_rg.location
   resource_group_name = azurerm_resource_group.dev_rg.name
 
+  # Allow SSH
   security_rule {
     name                       = "Allow-SSH"
     priority                   = 1000
@@ -74,9 +16,129 @@ resource "azurerm_network_security_group" "dev_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  # Allow HTTP Inbound
+  security_rule {
+    name                       = "Allow-HTTP-Inbound"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTP Outbound
+  security_rule {
+    name                       = "Allow-HTTP-Outbound"
+    priority                   = 1002
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow NodePort Range Inbound
+  security_rule {
+    name                       = "Allow-NodePort-Range-Inbound"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "30000-32767"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow NodePort Range Outbound
+  security_rule {
+    name                       = "Allow-NodePort-Range-Outbound"
+    priority                   = 1004
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "30000-32767"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTPS Inbound
+  security_rule {
+    name                       = "Allow-HTTPS-Inbound"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTPS Outbound
+  security_rule {
+    name                       = "Allow-HTTPS-Outbound"
+    priority                   = 1006
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow Port 8080 Inbound
+  security_rule {
+    name                       = "Allow-8080-Inbound"
+    priority                   = 1007
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow Port 8080 Outbound
+  security_rule {
+    name                       = "Allow-8080-Outbound"
+    priority                   = "1008"
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-# Create a Public IP
+# Virtual Network
+resource "azurerm_virtual_network" "dev_vnet" {
+  name                = "dev-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.dev_rg.location
+  resource_group_name = azurerm_resource_group.dev_rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "dev_subnet" {
+  name                 = "dev-subnet"
+  resource_group_name  = azurerm_resource_group.dev_rg.name
+  virtual_network_name = azurerm_virtual_network.dev_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
 resource "azurerm_public_ip" "dev_public_ip" {
   name                = "dev-public-ip"
   location            = azurerm_resource_group.dev_rg.location
@@ -85,7 +147,7 @@ resource "azurerm_public_ip" "dev_public_ip" {
   sku                 = "Standard"
 }
 
-# Create a Network Interface
+# Network Interface
 resource "azurerm_network_interface" "dev_nic" {
   name                = "dev-nic"
   location            = azurerm_resource_group.dev_rg.location
@@ -105,7 +167,7 @@ resource "azurerm_network_interface_security_group_association" "dev_nic_nsg" {
   network_security_group_id = azurerm_network_security_group.dev_nsg.id
 }
 
-# Create a Virtual Machine
+# Virtual Machine
 resource "azurerm_linux_virtual_machine" "dev_vm" {
   name                = "dev-vm"
   resource_group_name = azurerm_resource_group.dev_rg.name
@@ -133,14 +195,9 @@ resource "azurerm_linux_virtual_machine" "dev_vm" {
   }
 }
 
-# Define a variable for SSH public key
-variable "ssh_public_key" {
-  description = "SSH public key for VM access"
-  type        = string
-}
-
 # Outputs
 output "vm_public_ip" {
   value       = azurerm_public_ip.dev_public_ip.ip_address
   description = "The public IP of the VM"
 }
+
