198f37b0d0a3ccb4ffa4212f2601062e1cb7930e
snshah16 <sarthaknshah1998@gmail.com>
2024-11-07 09:10:26 -0500

    Update configure.tf removed creating resource group part

--- terraform_code/configure.tf
+++ terraform_code/configure.tf
@@ -1,36 +1,8 @@
-# Provider Configuration
-provider "azurerm" {
-  features {}
-}
-
-# Resource Group for the Backend State
-resource "azurerm_resource_group" "dev_rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Storage Account for the Terraform State
-resource "azurerm_storage_account" "backend_storage" {
-  name                     = "shreyas3799"         # Ensure this name is globally unique
-  resource_group_name      = azurerm_resource_group.dev_rg.name
-  location                 = azurerm_resource_group.dev_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# Storage Container for the Terraform State
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.backend_storage.name
-  container_access_type = "private"
-}
-
-# Configure Backend for Remote State Storage (uncomment after initial apply)
 terraform {
   backend "azurerm" {
-    resource_group_name   = azurerm_resource_group.dev_rg.name
-    storage_account_name  = azurerm_storage_account.backend_storage.name
-    container_name        = azurerm_storage_container.tfstate.name
+    resource_group_name   = "myBackendResourceGroup"   # Hardcoded name of the resource group
+    storage_account_name  = "shreyas3799"              # Hardcoded name of the storage account
+    container_name        = "tfstate"                  # Hardcoded name of the storage container
     key                   = "dev.tfstate"
   }
 }
