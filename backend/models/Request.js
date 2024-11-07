0f1a9e352c32a6fedd3326c1b0a6ab082265cba9
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2024-11-07 13:06:45 -0500

    Added the request date and comment in the schema

--- backend/models/Request.js
+++ backend/models/Request.js
@@ -6,6 +6,14 @@ const RequestSchema = new mongoose.Schema({
         required: true,
         enum: ['delivery', 'pick up'], // Limits to these two types
     },
+    requestDate: {
+        type: Date,
+        required: true,
+    },
+    comments: {
+        type: String,
+        required: false,
+    },
     userId: {
         type: String,
         required: true, // Ensure the request is tied to a specific user
