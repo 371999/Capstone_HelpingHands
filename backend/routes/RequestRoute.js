a4d1af36bf51de71585ea70a002b47971d476aea
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2024-11-14 19:39:31 -0500

    Added the route for update he info of item at  RequestRoute.js

--- backend/routes/RequestRoute.js
+++ backend/routes/RequestRoute.js
@@ -6,5 +6,6 @@ router.get('/:ownerId', getRequestsByUserId);
 router.get('/:itemId/:userId', getRequestByItemAndUser);
 router.post("/", createRequest);
 router.delete('/:id', deleteRequestById);
+router.patch('/:id/status', updateRequestStatus);
 
 module.exports = router;
