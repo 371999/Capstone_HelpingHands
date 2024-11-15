dd051d6d007f119285171852cdbc3568561659df
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2024-11-14 19:31:42 -0500

    The delete route added to RequestRoute.js

--- backend/routes/RequestRoute.js
+++ backend/routes/RequestRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/:ownerId', getRequestsByUserId);
 router.get('/:itemId/:userId', getRequestByItemAndUser);
 router.post("/", createRequest);
+router.delete('/:id', deleteRequestById);
 
 module.exports = router;
