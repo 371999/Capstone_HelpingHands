7366d0b25818a72d3dab3541b10a847f9df37069
snshah16 <sarthaknshah1998@gmail.com>
2024-11-19 14:26:12 -0500

    Update variable.tf

--- terraform_code/variable.tf
+++ terraform_code/variable.tf
@@ -14,7 +14,7 @@ variable "location" {
 variable "ssh_public_key" {
   description = "SSH public key for VM access"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCm2fv+47aFfPymooHhw+nKTm71F8njbHp0Dgkr8DZEQrZET8xLhYw/+nImXdJMa1a9uLPYFMmNqQh3P6de+S08ypbTdR0NMhNwRgBfJ8xJhPQjKRE5be2xVtu9jiodeUw4tObLncj04eK6jC9DKiNlRG19957hEHxxOP5KX84mXr/7oKqZjITZWd+8uDT3X1ehAzPdR8tiWkogmooj6BrcrvpybcMFdg4LDQvlAZEfV2hfGCZjeSw0DeZ8PfZMzASsrzRnCH9P9AYCc4DGrCE3oViV0Wiqkq2llwt2haaSm2CJO1IdySOhMODPXDDMcF1108qRmac2RmWfMFqNKNJr odl_user@SandboxHost-638675848769399033"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYdH26eVa6+/XTbJdhcj1qMkIT53f0zGu0Ch9ki6poXaGGzgc8WtqNZJOVkR1LnrBFPl8smPPcdxk8xpaKhz/dwLbZgjop08325OnuRN2hs9hPmZp6+v8lFANP5rcCUncBJ+NMy8ojdxE+66At7egdK4WYEGiDk51DCL6iMcMdEmFVKvE3nZ7eig6Jof8K5aBPza+R3giKcYFXdHNeeyvl9rj+5xYKgcQOvLe4jSMfsF+jDqkpkhkWF9KuekQuAXy8HAvGcJE6xolzwETj02Pb+6Y4ZyCWXDwRDI+WsGIWEAeE6fsoQV2+aHTYPG/9PmPDI6ECADGbsjerDghsHird odl_user@SandboxHost-638676322078001737"
 }
 variable "subscription_id" {
   description = "Azure subscription ID"
