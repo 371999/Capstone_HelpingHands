506b369ded2fb13c3b3dbe4637acfb78282c08b6
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-22 03:37:01 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,8 @@ resource "azurerm_public_ip" "frontend_backend_pip" {
   resource_group_name = azurerm_resource_group.frontend_backend_rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
+
+  domain_name_label   = "helpinghands"
 }
 
 # Creating Network Security Group (NSG)
@@ -157,3 +159,16 @@ resource "azurerm_container_registry" "frontend_backend_acr" {
   }
 }
 
+# DNS Zone and CNAME Record
+resource "azurerm_dns_zone" "helpinghands_org" {
+  name                = "helpinghands.org"
+  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
+}
+
+resource "azurerm_dns_cname_record" "www" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.helpinghands_org.name
+  resource_group_name = azurerm_resource_group.frontend_backend_rg.name
+  ttl                 = 300
+  record              = azurerm_public_ip.frontend_backend_pip.fqdn # Points to Azure DNS label
+}
