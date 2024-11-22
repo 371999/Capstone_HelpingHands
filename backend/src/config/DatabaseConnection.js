67f33b7bc27a57c4edf395aab91a237dc8ffe85e
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-22 00:35:30 -0500

    Update DatabaseConnection.js

--- backend/src/config/DatabaseConnection.js
+++ backend/src/config/DatabaseConnection.js
@@ -3,14 +3,11 @@ const mongoose = require('mongoose');
 
 const DatabaseConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database connection established successfully`);
     } catch (e) {
         console.error(`Database Connection Error: ${e.message}`);
     }
 };
 
-module.exports = {
-    connect: DatabaseConnection,
-    disconnect,
-};
+module.exports = DatabaseConnection;
