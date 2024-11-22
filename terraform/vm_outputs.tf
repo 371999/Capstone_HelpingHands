9d33606cdb121de30db1d1cb0a0864167bfafed3
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-22 05:54:14 -0500

    Update vm_outputs.tf

--- terraform/vm_outputs.tf
+++ terraform/vm_outputs.tf
@@ -2,8 +2,3 @@ output "frontend_backend_vm_id" {
   description = "The ID of the Virtual Machine running frontend and backend"
   value       = azurerm_linux_virtual_machine.frontend_backend_vm.id
 }
-
-output "public_ip_fqdn" {
-  value       = azurerm_public_ip.frontend_backend_pip.fqdn
-  description = "The Fully Qualified Domain Name (FQDN) of the public IP."
-}
