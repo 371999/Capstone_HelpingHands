e4aa6abd92fb07ea1ec36123f8d52cb3fdf85167
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-21 23:10:02 -0500

    Update vm_variables.tf

--- terraform/vm_variables.tf
+++ terraform/vm_variables.tf
@@ -2,3 +2,8 @@ variable "public_key" {
   description = "The public SSH key for the virtual machine"
   type        = string
 }
+
+variable "private_key" {
+  description = "The private SSH key for the virtual machine"
+  type        = string
+}
