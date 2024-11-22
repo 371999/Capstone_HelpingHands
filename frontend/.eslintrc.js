ce9b80b9c3e5bea0ef8bb09210549c716fb5ee41
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-22 00:18:14 -0500

    Create .eslintrc.js

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // Add your frontend-specific rules here
+  },
+};
