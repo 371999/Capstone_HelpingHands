b3be6be1dc270c6cab4aa859d8d50ee4a3801e13
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-22 04:03:19 -0500

    Update vm_outputs.tf

--- terraform/vm_outputs.tf
+++ terraform/vm_outputs.tf
@@ -7,3 +7,13 @@ output "public_ip_fqdn" {
   value       = azurerm_public_ip.frontend_backend_pip.fqdn
   description = "The Fully Qualified Domain Name (FQDN) of the public IP."
 }
+
+output "dns_zone_name" {
+  value       = azurerm_dns_zone.helpinghands_org.name
+  description = "The name of the DNS Zone."
+}
+
+output "cname_record_www" {
+  value       = azurerm_dns_cname_record.www.record
+  description = "The FQDN that 'www' subdomain points to."
+}
