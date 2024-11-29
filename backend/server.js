50e674ce797c0a6da41624013381daf78101bc38
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-11-29 07:07:48 -0500

    Delete backend/server.js

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/index'); // Import the app (or 'app.js' if you rename it)
-const SERVERPORT = process.env.PORT || 8080;
-
-// Start the server
-app.listen(SERVERPORT, () => {
-    console.log(`Server is running successfully on port ${SERVERPORT}`);
-});
